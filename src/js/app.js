283079b20d187524f488cb4bf3e1bb0dde30a0ff
srdjan <srdjan@timok.com>
2014-12-26 10:51:40 -0500

    0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,29 @@
-//
-//- app:     Omniscient-ToDo
+//- app:     Omniscient-ToDo-app
 //- author:  @djidja8
 //- version: v0.1
 //
 'use strict'
-
 var React     = require('react'),
     immstruct = require('immstruct'),
     component = require('omniscient')
 
-var todostore = immstruct({ items: [{checked: false, archived: false, text: 'Buy milk'}] })
+//-- todo data store
+//
+var todostore = immstruct({
+                    items: [{checked: false, archived: false, text: 'Buy milk'}]
+                })
 
+//-- todo component
+//
 var todoMixins = {
   onChecked() {
     this.props.todo.update('checked', state => !state)
   },
   handleEdit() {
-    // this.props.todo.update('checked', state => !state)
+    // todo
+  },
+  handleChange() {
+    // todo
   },
   onDestroy() {
     this.props.todo.set('archived', true);
@@ -26,15 +33,24 @@ var Todo = component(todoMixins, function() {
     return (
        <li id="todo-list" className={this.props.todo.get('checked') && 'completed'}>
         <div className="view">
-          <input className="toggle" type="checkbox" onChange={this.onChecked} checked={this.props.todo.get('checked')}/>
+          <input className="toggle"
+                 type="checkbox"
+                 onChange={this.onChecked}
+                 checked={this.props.todo.get('checked')}/>
           <label onDoubleClick={this.handleEdit}> {this.props.todo.get('text')} </label>
           <button className="destroy" onClick={this.onDestroy}/>
         </div>
-        <input className="edit"/>
+        <input className="edit"
+               value={this.props.todo}
+               onBlur={this.handleSubmit}
+               onChange={this.handleChange}
+               onKeyDown={this.handleKeyDown}/>
       </li>
     )}
 )
 
+//-- todolist component
+//
 var todoListMixins = {
   onChecked() {
     this.refs.checkAll.checked = !this.refs.checkAll.checked
@@ -44,30 +60,36 @@ var todoListMixins = {
 var TodoList = component(todoListMixins, function() {
     return (
         <section id="main">
-          <input id="toggle-all" type="checkbox" checked={false} onChange={this.onChecked} ref="checkAll"/>
+          <input id="toggle-all" type="checkbox"
+                                 checked={false}
+                                 onChange={this.onChecked}
+                                 ref="checkAll"/>
           <ul id="todo-list">
-            { this.props.todolist.map((i, indx) => i.get('archived') ? '' : <Todo key={indx} todo={i}/>).toArray() }
+            {
+              this.props.todolist.map((i, indx) =>
+                i.get('archived') ? '' : <Todo key={indx} todo={i}/>).toArray()
+            }
           </ul>
         </section>
     )}
 )
 
+//-- main component
+//
 var mainMixins = {
   onAdded(event) {
     if (event.key === 'Enter') {
-      event.preventDefault()
-
       var val = this.refs.text.getDOMNode().value.trim()
       if (val) {
-        this.props.todolist.update(items => items.push(immstruct({checked: false, archived: false, text: val}).current) )
+        this.props.todolist.update(items => items.push(
+                                   immstruct({checked: false, archived: false, text: val})
+                                   .current))
         this.refs.text.getDOMNode().value = ''
       }
     }
   },
   clearCompleted() {
-    // if(this.completed() > 0) {
-      this.props.todolist.update(items => items.filter(i => !i.get('checked')))
-    // }
+    this.props.todolist.update(items => items.filter(i => !i.get('checked')))
   },
   itemsLeft() {
     var completed = 0, archived = 0
@@ -103,11 +125,16 @@ var Main = component(mainMixins, function() {
             <li><a href="#/archived"> Archived </a></li>
           </ul>
           <button id="clear-completed" onClick={this.clearCompleted}> Clear Completed </button>
+          <div id="info">
+            <p>Created by <a href="http://twitter.com/djidja8/">djidja8</a></p>
+          </div>
         </footer>
       </div>
     )
 })
 
+//-- start
+//
 function render() {
   React.render(<Main todolist={todostore.cursor(['items'])}/>, document.body);
 }
